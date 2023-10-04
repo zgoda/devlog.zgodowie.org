6550150584c59d672ad7d868386c9d36fa137efe
Jarek Zgoda <jarek.zgoda@gmail.com>
2023-10-04 19:40:56 +0200

    Devtooling

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    node: true,
+    es2022: true,
+    browser: true,
+  },
+  extends: ['eslint:recommended', 'plugin:astro/recommended'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {},
+  overrides: [
+    {
+      files: ["*.astro"],
+      parser: "astro-eslint-parser",
+      parserOptions: {
+        extraFileExtensions: [".astro"],
+      },
+      rules: {},
+    },
+  ],
+};
