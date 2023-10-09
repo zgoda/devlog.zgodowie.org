f7acea208f126cc8bfc69412c5bbc17dcfeeb193
Jarek Zgoda <jarek.zgoda@gmail.com>
2023-10-09 23:59:48 +0200

    Reviving old content

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from 'astro/config';
 
-import preact from "@astrojs/preact";
+import preact from '@astrojs/preact';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [preact()]
+  integrations: [preact()],
+  markdown: {
+    shikiConfig: {
+      theme: 'nord',
+    },
+  },
 });
