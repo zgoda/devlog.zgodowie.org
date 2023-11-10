6da65ae61bdfa2e4e13f085ebf63b3bdb51c9ed2
Jarek Zgoda <jarek.zgoda@gmail.com>
2023-11-10 18:27:22 +0100

    New post: Trabant 1st impressions

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  prefetch: true,
   site: 'https://devlog.zgodowie.org',
   markdown: {
     shikiConfig: {
